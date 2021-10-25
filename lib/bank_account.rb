2f345fed54bf435df54dc0f2d3e4ba0a63652580
Shakira <shakira.r.salazar@gmail.com>
2021-10-25 13:26:14 +0100

    able to deposit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,26 @@
 class Bank_Account
 
-	attr_reader :balance
+	attr_reader :balance, :history
 
 	def initialize(balance = 0)
 		@balance = balance
+		@history = [[get_current_date, "", "", balance]]
+	end
+
+	def deposit(amount)
+		@balance += amount
+		@history.push([get_current_date, "", amount, @balance])
+	end
+
+	def withdraw(amount)
+		@balance -= amount
+		# @history.push([get_current_date, "", amount, @balance])
+	end
+
+	private
+
+	def get_current_date
+		Time.now.strftime("%d/%m/%Y")
 	end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -5,15 +5,46 @@ require 'bank_account'
 describe 'Bank account' do
 	
 	let(:account){Bank_Account.new}
+	let(:with_start_balance){Bank_Account.new(1500)}
 	
-	it 'should have a default balance of 0' do
-		test_balance = Bank_Account.new
-		expect(test_balance.balance).to eq 0
+	it 'Should have a default balance of 0' do
+		expect(account.balance).to eq 0
 	end
 
-	it 'should initialize with a balance of 1500' do
-		test_balance = Bank_Account.new(1500)
-		expect(test_balance.balance).to eq 1500
+	it 'Should initialize with a balance of 1500' do
+		expect(with_start_balance.balance).to eq 1500
+	end
+
+	it 'Should initialize with one entry in history' do
+		expect(account.history.length).to eq 1
+	end
+
+	describe 'Deposit' do
+		
+		it 'Should increase the balance by the amount entered' do
+			account.deposit(3000)
+			expect(account.balance).to eq 3000
+		end
+
+		it 'Should record the changes made' do
+			account.deposit(3000)
+			expect(account.history.length).to eq 2
+		end
+
+	end
+	
+	describe 'Withdraw' do
+		
+		it 'Should decrease the balance by the amount entered' do
+			with_start_balance.withdraw(300)
+			expect(with_start_balance.balance).to eq 1200
+		end
+
+		# it 'Should record the changes made' do
+		# 	account.deposit(3000)
+		# 	expect(account.history.length).to eq 2
+		# end
+
 	end
 
 end
